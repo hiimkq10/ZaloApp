e81d910914adfb054d956051d9f0db27b2a88384
thanhhai6398 <thanhhai6398@gmail.com>
2022-04-30 21:42:18 +0700

    Login_Regis_Info_Update

--- ZaloApp/app/src/main/java/hcmute/nhom2/zaloapp/ListChatAndContactActivity.java
+++ ZaloApp/app/src/main/java/hcmute/nhom2/zaloapp/ListChatAndContactActivity.java
@@ -1,16 +1,16 @@
 package hcmute.nhom2.zaloapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
